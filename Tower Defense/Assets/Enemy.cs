f13262210e9721f0420c4ba99bf83dc87a417930
alayu2020 <osupartpls@gmail.com>
2018-07-16 23:25:46 -0400

    Added enemy movement

---
+++ Tower Defense/Assets/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f){
+
+            GetNextWayPoint();
+
+        }
+
+    }
+    void GetNextWayPoint(){
+
+        if(wavePointIndex >= Waypoints.points.Length - 1){
+
+            Destroy(gameObject);
+            return;
+        }
+
+        wavePointIndex++;
+        target = Waypoints.points[wavePointIndex];
+
+    }
+}
