b175c701179b76a503edc9044eefb22e1d24f3f7
alayu2020 <osupartpls@gmail.com>
2018-07-22 12:04:03 -0400

    Updated Turrets and Shooting

---
+++ Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if(countdown <= 0f){
+
+            StartCoroutine(spawnWave());
+            countdown = timeBetweenWaves;
+
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator spawnWave(){
+
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++){
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+
+
+    }
+
+    void spawnEnemy(){
+
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+
+    }
+}
