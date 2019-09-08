a89971eed1216385da38420fcbbf0f33dc8cf6fd
alayu <alayu@umich.edu>
2019-09-08 12:48:44 -0400

    Added flappy bird

---
+++ FlappyBirdLive/Assets/Scripts/GameControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{   
+    public GameObject gameOverText;
+    public int gameOver = 0;
+    
+    
+    public static GameControl instance;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+    
+    private int score = 0;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        }else if (instance != this){
+            Destroy(gameObject);    
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == 2 && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))){
+            SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
+        }
+    }
+    
+    public void BirdScored(){
+        
+        
+        if(gameOver == 2){
+            return;
+        }else{
+         
+            score++;
+            scoreText.text = "Score: " + score.ToString();
+
+        }
+
+    }
+    
+    public void BirdDied(){
+        gameOver++;
+        if(gameOver == 2){
+            gameOverText.SetActive(true);
+        }
+    }
+}
---
+++ FlappyBirdLive/Assets/Scripts/bird.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    private bool isDead;
+    private Rigidbody2D rb2d;
+    private Animator anim;                    //Reference to the Animator component.
+    
+    public int player_number;
+    
+    public float upForce = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
+        
+        if(gameObject.CompareTag("p1")){
+            player_number = 0;
+        }else{
+            player_number = 1;
+        }
+        
+        Debug.Log(player_number.ToString());    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isDead){
+            
+            //Spacebar
+            if(player_number == 1){
+                
+                if (Input.GetKeyDown(KeyCode.Space)){
+                    anim.SetTrigger("Flap");
+
+                    rb2d.velocity = Vector2.zero;
+                    rb2d.AddForce(new Vector2(0, upForce));
+                }
+                
+            }else{
+                
+                //Left click
+                if (Input.GetMouseButtonDown(0)){
+                    anim.SetTrigger("Flap");
+
+                    rb2d.velocity = Vector2.zero;
+                    rb2d.AddForce(new Vector2(0, upForce));
+                }
+                
+            }
+        }
+    }
+    
+    void OnCollisionEnter2D(){
+        rb2d.velocity = Vector2.zero;
+        
+      
+        anim.SetTrigger ("Die");
+        
+        if(!isDead){
+            GameControl.instance.BirdDied ();
+        }
+        
+        isDead = true;
+
+
+    }
+}
